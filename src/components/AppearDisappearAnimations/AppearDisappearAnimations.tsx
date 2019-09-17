e8f3664db013772b95fd544ac78f7b4d262b7bfb
cody <cevek@ya.ru>
2019-09-17 08:53:46 +0000

    Fix mount/unmount animations

--- src/components/AppearDisappearAnimations/AppearDisappearAnimations.tsx
+++ src/components/AppearDisappearAnimations/AppearDisappearAnimations.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
-import AnimateMount from '../../lib/AnimateMount/AnimateMount';
+import AnimateMount, { AnimateUnmount } from '../../lib/AnimateMount/AnimateMount';
 
 export default function AppearDisappearAnimations() {
   const [clicked, click] = React.useState(false);
+  const [unmountClicked, unmountClick] = React.useState(false);
 
   return (
     <React.Fragment>
-      <h1>0. Mount/Unmount Animations</h1>
+      <h1>Mount Animations</h1>
       <button onClick={() => click(!clicked)}>click</button>
-      <AnimateMount show={clicked}>
-        <h1>first</h1>
-      </AnimateMount>
-      <AnimateMount show={!clicked}>
-        <h1>second</h1>
-      </AnimateMount>
+      {clicked ? (
+        <AnimateMount key={1}>
+          <h1>first</h1>
+        </AnimateMount>
+      ) : (
+        <AnimateMount key={2}>
+          <h1>second</h1>
+        </AnimateMount>
+      )}
+      <h1>Unmount Animations</h1>
+      <button onClick={() => unmountClick(!unmountClicked)}>click</button>
+      <AnimateUnmount show={!unmountClicked}>
+        <h1>first unmoumt</h1>
+      </AnimateUnmount>
+      <AnimateUnmount show={unmountClicked}>
+        <h1>second unmount</h1>
+      </AnimateUnmount>
     </React.Fragment>
   );
 }
--- src/lib/AnimateMount/AnimateMount.tsx
+++ src/lib/AnimateMount/AnimateMount.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { findDOMNode } from 'react-dom';
 
 type Props = {
   children: JSX.Element;
@@ -12,7 +13,7 @@ type State = {
   prevProps: Props;
 };
 
-export default class AnimateMount extends React.Component<Props, State> {
+export class AnimateMount2 extends React.Component<Props, State> {
   private wrapperRef = React.createRef<HTMLDivElement>();
 
   state: State = {
@@ -38,9 +39,7 @@ export default class AnimateMount extends React.Component<Props, State> {
     return null;
   }
 
-  private handleTransitionEnd = (
-    event: React.TransitionEvent<HTMLDivElement>
-  ) => {
+  private handleTransitionEnd = (event: React.TransitionEvent<HTMLDivElement>) => {
     this.props.onAnimateComplete && this.props.onAnimateComplete();
     if (event.target === this.wrapperRef.current && !this.props.show) {
       this.setState({
@@ -63,4 +62,48 @@ export default class AnimateMount extends React.Component<Props, State> {
       </div>
     );
   }
+}
+
+type AnimateMountProps = { children: React.ReactChild };
+export default class AnimateMount extends React.Component<AnimateMountProps> {
+  componentDidMount() {
+    const node = findDOMNode(this) as HTMLElement;
+    node.style.opacity = '0';
+    node.style.transform = 'translateY(20px)';
+    node.style.transition = 'transform .3s, opacity .3s linear';
+    forceReflow(node);
+    node.style.transform = '';
+    node.style.opacity = '';
+    node.addEventListener('transitionend', () => {
+      node.style.transition = '';
+    });
+  }
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
+
+type AnimateUnmountProps = { children: React.ReactChild; show: boolean };
+type AnimateUnmountState = { unMounted: boolean };
+export class AnimateUnmount extends React.Component<AnimateUnmountProps, AnimateUnmountState> {
+  state = { unMounted: false };
+  componentDidUpdate(prevProps: this['props']) {
+    if (!this.state.unMounted && prevProps.show && !this.props.show) {
+      const node = findDOMNode(this) as HTMLElement;
+      node.style.transition = 'opacity .3s';
+      forceReflow(node);
+      node.style.opacity = '0';
+      node.addEventListener('transitionend', () => {
+        this.setState({ unMounted: true });
+      });
+    }
+  }
+  render() {
+    return this.state.unMounted ? null : React.Children.only(this.props.children);
+  }
+}
+
+function forceReflow(node: HTMLElement) {
+  // eslint-disable-next-line no-unused-expressions
+  getComputedStyle(node).opacity;
 }
